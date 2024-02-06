ecdd839e31c13297dd73d2b4010ac0322659e818
Grey Boolean <greyboolean@gmail.com>
2024-02-06 20:02:10 +0530

    add userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		lowercase: true,
+		validate: [validator.isEmail],
+	},
+	password: {
+		type: String,
+		required: true,
+		select: false,
+	},
+	role: {
+		type: String,
+		enum: ["user", "admin"],
+		default: "user",
+	},
+});
+
+const user = mongoose.model("User", userSchema);
+
+module.exports = User;
